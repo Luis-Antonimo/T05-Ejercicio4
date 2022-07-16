8c849e54ea86981e8b176f7080b9f7da79353238
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-16 22:22:39 +0200

    ok
    ok

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T05Ejercicio4
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Escriba el número del que desea saber el factorial:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("El factorial de {0} es: {1}", numero, Factorial(numero));
+        }
+
+        static int Factorial(int numero)
+        {
+            int operacion = 1;
+            for (int i = 1; i <= numero; i++)
+            {
+                operacion *= i;
+            }
+            return operacion;
+        }
+
+    }
+}
